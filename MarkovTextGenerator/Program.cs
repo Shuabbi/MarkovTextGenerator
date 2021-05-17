a120601fc2e8cc31b8a81c899614d0e4037b9172
Joshua Averion <joshaver24@gmail.com>
2021-05-17 11:48:44 -0400

    Finished markov

--- MarkovTextGenerator/Program.cs
+++ MarkovTextGenerator/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace MarkovTextGenerator
 {
     class Program
@@ -12,42 +11,32 @@ namespace MarkovTextGenerator
         static void Main(string[] args)
         {
             Chain chain = new Chain();
-
             Console.WriteLine("Welcome to Marky Markov's Random Text Generator!");
-
-            Console.WriteLine("Enter some text I can learn from (enter single ! to finish): ");
-
-            // LoadText("Sample.txt", chain);
-
+            Console.WriteLine("Enter some text I can learn from (enter ! to finish): ");
+            //LoadText("Sample.txt", chain);
             while (true)
             {
-
                 Console.Write("> ");
-
                 String line = Console.ReadLine();
                 if (line == "!")
                     break;
-
                 chain.AddString(line);  // Let the chain process this string
             }
-
             // Now let's update all the probabilities with the new data
             chain.UpdateProbabilities();
-
             // Okay now for the fun part
-            Console.WriteLine("Done learning!  Now give me a word and I'll tell you what comes next.");
+            Console.WriteLine("Done learning!  Now give me a word and I'll generate a sentence.");
             Console.Write("> ");
-
-            String word = Console.ReadLine();
-            String nextWord = chain.GetNextWord(word);
-            Console.WriteLine("I predict the next word will be " + nextWord);
+            String word = chain.startingwords[0];
+            Console.WriteLine(chain.GenerateSentence(word));
+            String word2 = Console.ReadLine();
+            String nextWord3 = chain.GetNextWord(word);
+            Console.WriteLine("I predict the next word will be " + nextWord3);
         }
-
         static void LoadText(string filename, Chain chain)
         {
             int counter = 0;
             String line;
-
             string path = Path.Combine(Environment.CurrentDirectory, $"data\\{filename}");
             StreamReader file = new StreamReader(path);
             while ((line = file.ReadLine()) != null)
@@ -55,7 +44,6 @@ namespace MarkovTextGenerator
                 chain.AddString(line);
                 counter++;
             }
-
             file.Close();
         }
     }
